02087c1e53a1ac0572f46e816a9c964c99e03a81
溪 刘 <9tqqd2bkpb@privaterelay.appleid.com>
2021-06-08 19:37:16 +0800

    da

--- apps/soundcloud/src/app/app.component.spec.ts
+++ apps/soundcloud/src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have as title 'soundcloud'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('soundcloud');
+    expect(app.title).toEqual('soundcloud1');
   });
 
   it('should render title', () => {
